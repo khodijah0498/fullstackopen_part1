d8ca1a695257d272f4341552795a25d1407f9c2e
khodijah0498 <khodijahlawal@gmail.com>
2022-06-29 14:05:56 +0100

    1.2: course information, step2

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -9,6 +9,17 @@ const Header = ({course}) => {
 const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
   return (
     <div>
+      <Part part1 = {part1} exercises1 = {exercises1}/>
+      
+      <Part part2 = {part2} exercises2 = {exercises2} />
+      
+      <Part part3 = {part3} exercises3 = {exercises3} />
+    </div>
+  );
+}
+const Part = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+  return (
+    <div>
       <p>
         {part1} {exercises1}
       </p>
