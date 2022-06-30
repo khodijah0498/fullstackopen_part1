275ce2b19db1e40f775c387dbaddafddb0753737
khodijah0498 <khodijahlawal@gmail.com>
2022-06-30 15:40:34 +0100

    1.7: unicafe step2

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,19 +1,19 @@
-
-import {useState} from 'react';
+import { useState } from "react";
 const App = () => {
-  
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const addGood = () =>{
-    setGood(good + 1)
-  }
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const sum = good + neutral + bad;
+  const average = (good - bad) / sum;
+  const positive = `${(good / sum) * 100}%`;
+  const addGood = () => {
+    setGood(good + 1);
+  };
   const addNeutral = () => {
-     
-    setNeutral(neutral + 1)
+    setNeutral(neutral + 1);
   };
   const addBad = () => {
-      setBad(bad + 1)
+    setBad(bad + 1);
   };
 
   return (
@@ -26,7 +26,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {sum}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
-  )
-  }
+  );
+};
 export default App;
