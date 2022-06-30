46a1f8a383f607d6fbbc5d5d1ecc66cd7fed1213
khodijah0498 <khodijahlawal@gmail.com>
2022-06-30 15:41:22 +0100

    1.7: unicafe step2

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -26,7 +26,7 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {sum}</p>
+      <p>all {sum}</p> 
       <p>average {average}</p>
       <p>positive {positive}</p>
     </div>
