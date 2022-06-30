4ff584f4730c4df679b1b188925504b082925e79
khodijah0498 <khodijahlawal@gmail.com>
2022-06-30 18:06:14 +0100

    1.10: unicafe step5

--- unicafe/src/Statistics.js
+++ unicafe/src/Statistics.js
@@ -1,12 +1,14 @@
+import StatisticLine from "./StatisticLine";
 const Statistics = (props) => {
+const [good, neutral, bad, sum, average, positive] = props;
     return (
       <div>
-        <p>good {props.good}</p>
-        <p>neutral {props.neutral}</p>
-        <p>bad {props.bad}</p>
-        <p>all {props.sum}</p>
-        <p>average {props.average}</p>
-        <p>positive {props.positive}</p>
+        <StatisticLine text ="good" value ={good} />
+        <StatisticLine text ="neutral" value = {neutral} />
+        <StatisticLine text ="bad" value ={bad} />
+        <StatisticLine text = "all" value ={sum} />
+        <StatisticLine text = "average" value ={average} />
+        <StatisticLine text = "positive" value ={positive} />
       </div>
     );
 }
