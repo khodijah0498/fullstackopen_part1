74733d95415978a6daac0d8a836ce5c4f62fa1c8
khodijah0498 <khodijahlawal@gmail.com>
2022-07-01 18:10:15 +0100

    1.14*: anecdotes step3

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -11,29 +11,36 @@ const App = () => {
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
   ];
-  const score = new Array(7).fill(0)
-const [selected, setSelected] = useState(0);
-const [vote, setVote] = useState(score);
-const randomAnecdotes = () => {
-  setSelected(Math.floor(Math.random() * anecdotes.length));
-};
-const handleVote = () => {
-  const points = [...vote]
-  points[selected] = points[selected] + 1
-  setVote(points)
-}
+   const score = new Array(7).fill(0)
+  const [selected, setSelected] = useState(0);
+  const [vote, setVote] = useState(score);
+  const highestVote = Math.max(...vote);
+   const highestVoteIndex = vote.indexOf(highestVote);
 
-
+  const randomAnecdotes = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+  const handleVote = () => {
+    const points = [...vote];
+    points[selected] = points[selected] + 1;
+    setVote(points);
+  };
+  const highestAnecdotes = anecdotes.filter((anecdotes, aIndex)=> vote[aIndex]===highestVote);
+  // console.log("first highest,", anecdotes[highestVoteIndex]);
+  console.log(highestAnecdotes)
 
   return (
     <div>
+      <p>Anecdotes of the day</p>
       <div>{anecdotes[selected]}</div>
-      <p>has {vote[selected]} vote</p>
+      <p>has {vote[selected]} votes</p>
       <button onClick={handleVote}>vote</button>
       <button onClick={randomAnecdotes}>next anecdotes</button>
+      <p>Anecdotes with must votes</p>
+      <p>{anecdotes[highestVoteIndex]}</p>
+      <p>has {vote[selected]} votes</p>
     </div>
   );
-  
-};
+};;
 
 export default App;
